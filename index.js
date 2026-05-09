31dba2e187b749d8f8106c5958c5ddc5bf0fd5af
saul1217 <a1125250007@utch.edu.mx>
2026-05-09 17:54:12 -0600

    hola

--- index.js
+++ index.js
@@ -1,95 +1,27 @@
-//importa la libreria
 const express = require('express');
+const mysql = require('mysql2/promise');
+const cors = require('cors');
 
-//intanciar la app
 const app = express();
 
-//aca le decimos middelware para json
-app.use(express.json())
 
-//configurar el puerto (Por convicion es el 3000)
+app.use(express.json()); 
+app.use(cors());         
+
 const PORT = 3014;
 
-//definir de las rutas
-// req= request (Lo que viene del cliente)
-// res= response (Lo que enviamos del lado back)
-app.get('/', (req, res) => {
-    res.send('Hola mundo desde back')
-})
-
-let users = [{ id: 1, nombre: 'Chris' }]
-
-app.get('/users', (req, res) => res.json(users))
-
-app.post('/users', (req, res) => {
-    const {nombre} = req.body
-
-    if(!nombre || nombre.trim()===''){
-        return res.status(400).json({
-            error:"El nombre es obligatorio"
-        })
-    }
-
-    const nuevo = {id: users.length+1, nombre}
-    users.push(nuevo);
-    res.status(201).json(nuevo)
-})
-
-
-
-
-app.delete('/users/:id', (req, res) => {
-    const idDelete = parseInt(req.params.id)
-    const index = users.findIndex(u => u.id === idDelete)
-
-    if (index === -1) {
-        return res.status(404).json({
-            error: "Usuario no encontrado",
-            message: `No existe un usuario con el id ${idDelete}`
-        })
-    }
-
-    const userEliminado = users.splice(index, 1);
-
-
-    res.json({
-        message: "Usuario eliminado",
-        user: userEliminado[0]
-    })
-})
-
-app.get('/users/:id', (req, res) => {
-    const idDelete = parseInt(req.params.id)
-    const index = users.find((u) => u.id === id)
-
-    if (!users) {
-        return res.status(404).json({
-            error: "Usuario no encontrado"
-        })
-    }
-
-    res.json(user)
-})
-
-
-
-
-app.put("/users/:id", (req, res) => {
-  const id = parseInt(req.params.id);
-  const index = users.findIndex((u) => u.id === id);
-    if (index === -1) {
-    return res.status(404).json({
-      error: "Usuario no encontrado"
-    });
-  }
-    users[index] = { id, ...req.body };
-    res.json({
-        message: "Usuario actualizado",
-        user: users[index]
-    });
+const db = mysql.createPool({
+    host: 'localhost',
+    user: 'root',            
+    password: '',             
+    database: 'gym'
 });
 
+app.get('/', (req, res) => {
+    res.send('holaaaa');
+});
 
+// Arrancar el servidor
 app.listen(PORT, () => {
-    console.log(`Servidor escuchando en http://localhost:${PORT}`)
-})
+    console.log(`Backend escuchando en http://localhost:${PORT}`);
+});
